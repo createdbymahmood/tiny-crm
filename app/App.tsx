0f04459dea8cde56b9b420005bc77c10b5e609b8
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-06-03 12:39:43 +0330

    feat: :sparkles: setup ant design

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,9 @@
-const App = () => {
-    return <div />;
+import { themeConfig } from '@lib/ant-design';
+import { ConfigProvider } from 'antd';
+import * as React from 'react';
+
+const App: React.FC = () => {
+    return <ConfigProvider theme={themeConfig} />;
 };
 
 export default App;
---
+++ lib/ant-design.ts
@@ -0,0 +1,8 @@
+import type { ThemeConfig } from 'antd';
+
+export const themeConfig: ThemeConfig = {
+    token: {
+        fontFamily: 'Inter',
+        colorPrimary: '#000000',
+    },
+};
