466a51b6db923a3434aa4172a8be2bc6ebb1aaa3
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-06-04 18:52:56 +0330

    enable compression

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import viteCompression from 'vite-plugin-compression';
 
 const viteConfig = defineConfig({
-    plugins: [react(), checker({ typescript: true }), tsconfigPaths()],
+    plugins: [
+        react(),
+        checker({ typescript: true }),
+        tsconfigPaths(),
+        viteCompression(),
+    ],
     server: {
         port: 9900,
     },
