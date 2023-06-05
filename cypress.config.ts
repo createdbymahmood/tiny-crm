63c1396393ffc6b94fd1d9b6250134c49d0aad4b
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-06-05 14:17:47 +0330

    cleanup cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
         specPattern: 'cypress/tests/**/*.spec.{js,jsx,ts,tsx}',
         baseUrl: 'http://localhost:9900',
     },
-    env: {},
 });
