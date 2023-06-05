69f18f86a4564d21974ab8e17530a1841a25f16d
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-06-05 23:07:20 +0330

    fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,22 @@
-const {
-  init
-} = require('@fullstacksjs/eslint-config/init');
+const { init } = require('@fullstacksjs/eslint-config/init');
+
 module.exports = init({
-  modules: {
-    auto: true,
-    esm: true,
-    typescript: {
-      parserProject: ['./tsconfig.eslint.json'],
-      resolverProject: ['./tsconfig.json']
-    }
-  },
-  root: true,
-  settings: {
-    'import/resolver': {
-      typescript: {}
-    }
-  },
-  rules: {
-    'import/no-cycle': ['off'],
-    'cypress/unsafe-to-chain-command': ['off']
-  }
+    modules: {
+        auto: true,
+        esm: true,
+        typescript: {
+            parserProject: ['./tsconfig.eslint.json'],
+            resolverProject: ['./tsconfig.json'],
+        },
+    },
+    root: true,
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+    rules: {
+        'import/no-cycle': ['off'],
+        'cypress/unsafe-to-chain-command': ['off'],
+    },
 });
-export const extends = ['plugin:storybook/recommended'];
