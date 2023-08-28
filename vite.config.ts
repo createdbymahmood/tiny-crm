d88d40dbc10cfcb37924af79df86de70d9c3e6f3
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-08-28 11:37:02 +0330

    spare all chunks

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import checker from 'vite-plugin-checker';
 import viteCompression from 'vite-plugin-compression';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const getManualChunks = (id: string) => {
+    if (!id.includes('node_modules')) return;
+    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+};
+
 const viteConfig = defineConfig({
     plugins: [
         react(),
@@ -23,6 +28,13 @@ const viteConfig = defineConfig({
         include: ['app/**/*.{spec,test}.{j,t}s?(x)'],
     },
     envPrefix: 'APP',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: getManualChunks,
+            },
+        },
+    },
 });
 
 export default viteConfig;
