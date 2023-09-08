211dd29d3d68c27fa1137af3180cad355ff3818e
Mahmood Bagheri <createdbymahmood@gmail.com>
2023-09-08 22:27:16 +0330

    fix casing

---
+++ app/components/customer/DeleteCustomerPopconfirm/DeleteCustomerPopconfirm.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { DeleteCustomerPopconfirm } from './DeleteCustomerPopconfirm';
+
+const meta: Meta<typeof DeleteCustomerPopconfirm> = {
+    component: DeleteCustomerPopconfirm,
+};
+
+export const Default: StoryObj = {
+    name: 'Default DeleteCustomerPopconfirm',
+    args: {
+        open: true,
+        customerIds: [],
+    },
+};
+
+export default meta;
---
+++ app/components/customer/DeleteCustomerPopconfirm/DeleteCustomerPopconfirm.tsx
@@ -0,0 +1,55 @@
+import * as testIds from '@lib/cypress/testIds';
+import { useDeleteCustomersMutation } from '@lib/data-provider/services/customer';
+import { getTestAttributes } from '@utils/test/getTestAttributes';
+import { toClientErrorMessage } from '@utils/toClientErrorMessage';
+import type { PopconfirmProps } from 'antd';
+import { message, Popconfirm } from 'antd';
+import { head } from 'lodash';
+import * as React from 'react';
+
+const useDeleteCustomerPopConfirmState = () => {
+    const [deleteCustomers] = useDeleteCustomersMutation();
+
+    const onDeleteCustomer = async (customerIds: string[]) => {
+        try {
+            void message.loading(`Deleting customer(s)`);
+            await deleteCustomers(customerIds).unwrap();
+            void message.success(`Customer(s) deleted`);
+        } catch (error) {
+            void message.error(toClientErrorMessage(error));
+        }
+    };
+
+    return {
+        onDeleteCustomer,
+    };
+};
+
+export interface DeleteCustomerPopConfirmProps
+    extends Omit<PopconfirmProps, 'description' | 'title'> {
+    customerIds: string[];
+}
+
+export const DeleteCustomerPopconfirm: React.FC<
+    DeleteCustomerPopConfirmProps
+> = ({ customerIds, onConfirm, ...props }) => {
+    const { onDeleteCustomer } = useDeleteCustomerPopConfirmState();
+
+    return (
+        <Popconfirm
+            {...props}
+            placement='topRight'
+            title='Delete customer(s)'
+            description='Are you sure to delete selected customer(s)?'
+            onConfirm={e => {
+                onConfirm?.(e);
+                void onDeleteCustomer(customerIds);
+            }}
+            okText='Yes'
+            cancelText='No'
+            okButtonProps={getTestAttributes(
+                testIds.deleteCustomer.cta(head(customerIds)!),
+            )}
+        />
+    );
+};
