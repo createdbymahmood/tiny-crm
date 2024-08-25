3f9e8fb30b940b4c79720b760210d864c728c0f2
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-08-25 18:35:03 +0330

    chore: use colorPrimary from theme inside login form Logo SVG

--- src/app/auth/login-form/login-form-view.tsx
+++ src/app/auth/login-form/login-form-view.tsx
@@ -1,6 +1,6 @@
 import {LockOutlined, MailOutlined} from '@ant-design/icons';
 import {Button, Form, Input, Typography} from 'antd';
-import {createStyles} from 'antd-style';
+import {createStyles, useTheme} from 'antd-style';
 
 const useLoginFormViewStyles = createStyles(({token, responsive}) => ({
   container: {
@@ -45,20 +45,26 @@ interface LoginFormViewProps {
   isLoading: boolean;
 }
 
-const Logo = () => (
-  <svg
-    width='25'
-    height='24'
-    viewBox='0 0 25 24'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-  >
-    <rect x='0.464294' width='24' height='24' rx='4.8' fill='#1890FF' />
-    <path d='M14.8643 3.6001H20.8643V9.6001H14.8643V3.6001Z' fill='white' />
-    <path d='M10.0643 9.6001H14.8643V14.4001H10.0643V9.6001Z' fill='white' />
-    <path d='M4.06427 13.2001H11.2643V20.4001H4.06427V13.2001Z' fill='white' />
-  </svg>
-);
+const Logo = () => {
+  const {colorPrimary} = useTheme();
+  return (
+    <svg
+      width='25'
+      height='24'
+      viewBox='0 0 25 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect x='0.464294' width='24' height='24' rx='4.8' fill={colorPrimary} />
+      <path d='M14.8643 3.6001H20.8643V9.6001H14.8643V3.6001Z' fill='white' />
+      <path d='M10.0643 9.6001H14.8643V14.4001H10.0643V9.6001Z' fill='white' />
+      <path
+        d='M4.06427 13.2001H11.2643V20.4001H4.06427V13.2001Z'
+        fill='white'
+      />
+    </svg>
+  );
+};
 
 export const LoginFormView: React.FC<LoginFormViewProps> = ({isLoading}) => {
   const {styles} = useLoginFormViewStyles();
